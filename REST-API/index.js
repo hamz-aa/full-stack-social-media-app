dde30300aaf4a25ba3942f108176946efabf1705
Hamza <jhamzaahmed@gmail.com>
2024-03-18 16:04:03 +0500

    server side storage

--- REST-API/index.js
+++ REST-API/index.js
@@ -6,6 +6,8 @@ import morgan from "morgan";
 import userRoute from "./routes/users.js";
 import authRoute from "./routes/auth.js";
 import postRoute from "./routes/posts.js";
+import multer from "multer";
+import path from "path";
 
 dotenv.config();
 
@@ -14,10 +16,34 @@ const PORT = process.env.PORT || 8800;
 
 mongoose.connect(process.env.MONGO_URL);
 
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
 // middlewares
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("File uploaded successfully");
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
